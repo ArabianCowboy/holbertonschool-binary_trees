29fc93e245623afb77e82a66c15c870a62f97031
Arabian <midoxp@gmail.com>
2025-10-23 13:53:03 +0000

    Fix Task 1 compile: remove duplicate binary_tree_node definition

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,36 +2,15 @@
 
 /**
  * binary_tree_node - creates a binary tree node
- * @parent: parent pointer
- * @value: value to put in the new node
- * Return: pointer to the new node, or NULL on failure
- */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
-{
-	binary_tree_t *node = malloc(sizeof(binary_tree_t));
-
-	if (!node)
-		return (NULL);
-	node->n = value;
-	node->parent = parent;
-	node->left = NULL;
-	node->right = NULL;
-	return (node);
-}
-#include "binary_trees.h"
-
-/**
- * binary_tree_node - creates a binary tree node
  * @parent: pointer to the parent node
- * @value: integer to store in the new node
+ * @value: value to store in the new node
  *
  * Return: pointer to the new node, or NULL on failure
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
 		return (NULL);
 
