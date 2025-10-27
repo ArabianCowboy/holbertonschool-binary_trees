ee29199e1510d2a26ec9a82886d4ed5df00b2b67
OWNALGARNI <s.xooxs@hotmail.com>
2025-10-27 10:05:27 +0000

    Task 15:fix betty coding style

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -3,18 +3,19 @@
 /**
  * binary_tree_is_full - Checks if a binary tree is full
  * @tree: Pointer to the root node of the tree
- * Return: 1 if the tree is full, 0 otherwise or if tree is NULL
+ * Return: 1 if the tree is full, 0 if NULL or not full
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
+if (tree == NULL)
+return (0);
 
-    if (tree->left == NULL && tree->right == NULL)
-        return (1);
+if (tree->left == NULL && tree->right == NULL)
+return (1);
 
-    if (tree->left != NULL && tree->right != NULL)
-        return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+if (tree->left != NULL && tree->right != NULL)
+return (binary_tree_is_full(tree->left) &&
+binary_tree_is_full(tree->right));
 
-    return (0);
+return (0);
 }
