554c8cd103c4213310dfde0d5ddc6680b8f990cd
OWNALGARNI <s.xooxs@hotmail.com>
2025-10-28 07:52:30 +0000

    Task:fix compile

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,24 +8,24 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int left_height, right_height;
+int left_height, right_height;
 
-	if (tree == NULL)
-		return (0);
+if (tree == NULL)
+return (0);
 
-	if (tree->left == NULL && tree->right == NULL)
-		return (1);
+if (tree->left == NULL && tree->right == NULL)
+return (1);
 
-	if (tree->left == NULL || tree->right == NULL)
-		return (0);
+if (tree->left == NULL || tree->right == NULL)
+return (0);
 
-	left_height = (int)binary_tree_height(tree->left);
-	right_height = (int)binary_tree_height(tree->right);
+left_height = (int)binary_tree_height(tree->left);
+right_height = (int)binary_tree_height(tree->right);
 
-	if (left_height == right_height &&
-	    binary_tree_is_perfect(tree->left) &&
-	    binary_tree_is_perfect(tree->right))
-		return (1);
+if (left_height == right_height &&
+binary_tree_is_perfect(tree->left) &&
+binary_tree_is_perfect(tree->right))
+return (1);
 
-	return (0);
+return (0);
 }
